ffad061d5d70b745d6c6d73ae3cf5b1e83b60935
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-02-01 20:53:21 +0100

    Add RUBY_VERSION checks to avoid constant loading errors on ruby 1.8

--- config/initializers/encoding.rb
+++ config/initializers/encoding.rb
@@ -1,2 +1,4 @@
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+if RUBY_VERSION.match /1\.9/
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,2 +1,5 @@
 Haml::Template.options[:format] = :html5
-Haml::Template.options[:encoding] = 'utf-8'
+
+if RUBY_VERSION.match /1\.9/
+  Haml::Template.options[:encoding] = 'utf-8'
+end
