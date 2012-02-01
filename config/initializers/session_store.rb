9e32699be552890a52d3403e1719ef11f840e627
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-02-01 20:20:18 +0100

    Renamed application. #TROTTEL

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-MittagsplanerMobile::Application.config.session_store :cookie_store, :key => '_mittagsplaner-mobile_session'
+RailsBootstrap::Application.config.session_store :cookie_store, :key => '_mittagsplaner-mobile_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# MittagsplanerMobile::Application.config.session_store :active_record_store
+# RailsBootstrap::Application.config.session_store :active_record_store
