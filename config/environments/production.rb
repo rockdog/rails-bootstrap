3523939fe6ba639746c71c3cb5f5d69b0dceeb3e
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-02-01 20:15:57 +0100

    Fixed tagged logging. #FIX

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ MittagsplanerMobile::Application.configure do
   config.log_tags = [ :uuid ]
 
   # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
